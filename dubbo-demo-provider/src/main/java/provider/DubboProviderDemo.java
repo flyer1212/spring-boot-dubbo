e203f968695a0f70236aad71a18f0d49bd44c6b6
liuZOZO <308762585@qq.com>
2019-08-07 21:22:40 +0800

    dev

--- dubbo-demo-provider/src/main/java/provider/DubboProviderDemo.java
+++ dubbo-demo-provider/src/main/java/provider/DubboProviderDemo.java
@@ -2,11 +2,9 @@ package provider;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class DubboProviderDemo {
     public static void main(String[] args) {
         SpringApplication.run(DubboProviderDemo.class, args);
